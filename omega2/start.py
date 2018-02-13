1a58c46a0aad8be28bbee721e0a75218a7bb8000
Sergey Bendas <seregabendas@gmail.com>
2018-02-13 23:42:44 +0200

    Fixed bug, print time in console, loop time 1 -> 0.1

--- omega2/start.py
+++ omega2/start.py
@@ -9,18 +9,19 @@ sensor = device.Device(20.8, 20.8)
 ### USE FOR LOOP ###
 while True:
 ### ### ### #### ###
-    time.sleep(1)
+    time.sleep(0.1)
     sensor.update()
     
     if (sensor.isFingerIn()):        
         if (not sensor.isActive()):
             sensor.activate()            
         print(" ------------ ")
-        print("red      = " + sensor.getRed())
-        print("ir         = " + sensor.getIR())
-        print("ShO2  = " + sensor.getShO2())
+        print("red      = " + str(sensor.getRed()))
+        print("ir         = " + str(sensor.getIR()))
+        print("ShO2  = " + str(sensor.getShO2()))
         
     else:
+        print(time.strftime("%H : %M"))
         if (sensor.isActive()):
             sensor.disactivate()
         
